1484f15740b4fdcf6a7a3963f5fdba8277b51d35
EdisonJwa <zuohaozheng@hotmail.com>
2018-11-05 17:08:13 +0900

    finish error_report_zh-CN

--- error_report_zh-CN.ts
+++ error_report_zh-CN.ts
@@ -5,25 +5,25 @@
     <name>ErrorReport</name>
     <message>
         <source>TeamSpeak 3 Error</source>
-        <translation type="unfinished"></translation>
+        <translation type="TeamSpeak 3 出错"></translation>
     </message>
     <message>
         <source>Do you want to send the error report?</source>
-        <translation type="unfinished"></translation>
+        <translation type="要发送错误报告吗？"></translation>
     </message>
     <message>
         <source>The TeamSpeak 3 client has crashed. There is a crash report which will help
 the developers to fix the problem. Please type a description in the text
 field to reproduce the issue.</source>
-        <translation type="unfinished"></translation>
+        <translation type="TeamSpeak 3客户端崩溃了。软件生成了一份崩溃报告，它将会帮助开发人员解决问题。请输入说明以便于重现此问题。"></translation>
     </message>
     <message>
         <source>Thank you for your report!</source>
-        <translation type="unfinished"></translation>
+        <translation type="感谢您的报告！"></translation>
     </message>
     <message>
         <source>An error occured while transmitting the data.</source>
-        <translation type="unfinished"></translation>
+        <translation type="传输数据时发生错误。"></translation>
     </message>
 </context>
 </TS>
