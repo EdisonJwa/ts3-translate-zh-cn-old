b4c6a35b698083643fbec892e9d39b6f0af6a75a
EdisonJwa <zuohaozheng@hotmail.com>
2018-11-05 16:59:48 +0900

    finish update_zh-CN

--- update_zh-CN.ts
+++ update_zh-CN.ts
@@ -17,11 +17,11 @@
     </message>
     <message>
         <source>Beta</source>
-        <translation type="Beta"></translation>
+        <translation type="Beta 版"></translation>
     </message>
     <message>
         <source>Alpha</source>
-        <translation type="Alpha"></translation>
+        <translation type="Alpha 版"></translation>
     </message>
     <message>
         <source>Update to TeamSpeak %1</source>
@@ -29,96 +29,96 @@
     </message>
     <message>
         <source>You are currently on &lt;strong&gt;%1 channel&lt;/strong&gt;.</source>
-        <translation type="您当前在 &lt;strong&gt;%1 频道&lt;/strong&gt;."></translation>
+        <translation type="您当前在 &lt;strong&gt;%1 更新频道&lt;/strong&gt;."></translation>
     </message>
     <message>
         <source>Step one: Updating updater</source>
-        <translation type="unfinished"></translation>
+        <translation type="第一步：更新更新程序"></translation>
     </message>
     <message>
         <source>Step two: Updating TeamSpeak client</source>
-        <translation type="unfinished"></translation>
+        <translation type="第二步：更新 Teamspeak 客户端"></translation>
     </message>
     <message>
         <source>Confirm reboot</source>
-        <translation type="unfinished"></translation>
+        <translation type="确认重启"></translation>
     </message>
     <message>
         <source>&lt;p&gt;A computer restart is required to proceed with the client update. If you do not reboot now, the TeamSpeak update will be cancelled.&lt;/p&gt;&lt;p&gt;Do you want to reboot now?&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="&lt;p&gt;需要重新启动计算机才能继续进行客户端更新。 如果您现在不重新启动，TeamSpeak 更新将被取消。&lt;/p&gt;&lt;p&gt;您想现在重启吗？&lt;/p&gt;"></translation>
     </message>
     <message>
         <source>Question</source>
-        <translation type="unfinished"></translation>
+        <translation type="问题"></translation>
     </message>
     <message>
         <source>&lt;p&gt;Update in progress.&lt;/p&gt;&lt;p&gt;Are you sure you want to abort?&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="&lt;p&gt;正在更新。&lt;/p&gt;&lt;p&gt;您确定要中止吗？&lt;/p&gt;"></translation>
     </message>
     <message>
         <source>Waiting until TeamSpeak 3 Client has been closed.</source>
-        <translation type="unfinished"></translation>
+        <translation type="等到 TeamSpeak 3客户端关闭。"></translation>
     </message>
     <message>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation type="警告"></translation>
     </message>
     <message>
         <source>The TeamSpeak 3 Client should be
 closed before starting the update.</source>
-        <translation type="unfinished"></translation>
+        <translation type="开始更新前，请关闭 Teamspeak 3客户端。"></translation>
     </message>
     <message>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation type="信息"></translation>
     </message>
     <message>
         <source>&lt;p&gt;This is the last TeamSpeak version available for Windows Vista.&lt;/p&gt;&lt;p&gt;To upgrade TeamSpeak and take advantage of the latest features and bug fixes, please update your operating system to Windows 7 or newer.&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="&lt;p&gt;这是适用于 Windows Vista 的最新 TeamSpeak 版本。&lt;/p&gt;&lt;p&gt;要升级 Teamspeak 并获得最新功能和错误修复，请您将操作系统更新到 Windows 7或更高版本&lt;/p&gt;"></translation>
     </message>
     <message>
         <source>%1 KB / %2 KB</source>
-        <translation type="unfinished"></translation>
+        <translation type="%1 KB / %2 KB"></translation>
     </message>
     <message>
         <source>%1 KB/s</source>
-        <translation type="unfinished"></translation>
+        <translation type="%1 KB/s"></translation>
     </message>
     <message>
         <source>Update cancelled</source>
-        <translation type="unfinished"></translation>
+        <translation type="更新已取消"></translation>
     </message>
     <message>
         <source>Update</source>
-        <translation type="unfinished"></translation>
+        <translation type="更新"></translation>
     </message>
     <message>
         <source>Update cancelled. Push &quot;Details&quot; to check reasons.</source>
-        <translation type="unfinished"></translation>
+        <translation type="更新已取消。点击 &quot;详情&quot; 检查原因"></translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation type="错误"></translation>
     </message>
     <message>
         <source>Failed to query latest client
 version from update server.</source>
-        <translation type="unfinished"></translation>
+        <translation type="检查客户端最新版本失败"></translation>
     </message>
     <message>
         <source>Update server returned no version</source>
-        <translation type="unfinished"></translation>
+        <translation type="更新服务器没有返回版本信息"></translation>
     </message>
     <message>
         <source>Update server returned invalid version nr (%1)</source>
-        <translation type="unfinished"></translation>
+        <translation type="更新服务器返回无效版本号 nr (%1)"></translation>
     </message>
     <message>
         <source>Invalid version nr (%1)</source>
-        <translation type="unfinished"></translation>
+        <translation type="无效版本号 nr (%1)"></translation>
     </message>
 </context>
 </TS>
